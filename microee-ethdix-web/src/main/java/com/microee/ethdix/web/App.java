5735c329793be55ca4e18a5c83345db5936382a3
zhangchunhui <chunhui2001@gmail.com>
2021-01-19 00:30:46 +0800

    a
    a

--- microee-ethdix-web/src/main/java/com/microee/ethdix/web/App.java
+++ microee-ethdix-web/src/main/java/com/microee/ethdix/web/App.java
@@ -1,13 +1,20 @@
 package com.microee.ethdix.web;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import com.microee.stacks.starter.MainApp;
+
+@EnableDiscoveryClient
+@EnableAutoConfiguration()
+@ComponentScan(basePackages = {"com.microee"})
+@EnableFeignClients(basePackages = {"com.microee.**.rmi"})
+@SpringBootApplication
+//@RedisEnabled()
+public class App extends MainApp {
+    public static void main(String[] args) {
+        startup(App.class, args);
     }
 }
