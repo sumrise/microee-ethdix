10ad2f44fc55dabd9288917ce1a35d8f793df4ce
zhangchunhui <zhangchunhui@huobi.com>
2021-01-19 21:52:27 +0800

    a
    a

--- uniswap-dixx/routers/univ2/univ2-trade.routes.config.ts
+++ uniswap-dixx/routers/univ2/univ2-trade.routes.config.ts
@@ -2,9 +2,10 @@ import { CommonRoutesConfig } from '../../common/common.routes.config';
 import express from 'express';
 import debug from 'debug';
 import { expect } from 'chai';
-import { ChainId, Route, Token, TokenAmount, TradeType, Trade, WETH, Percent, JSBI } from '@uniswap/sdk';
+import { ChainId, Route, Token, TokenAmount, TradeType, Trade, WETH, Percent, JSBI, CurrencyAmount } from '@uniswap/sdk';
 import { ethers } from 'ethers';
 import { parseUnits } from '@ethersproject/units';
+import web3 from 'web3';
 
 import { getTokenDataByAddr, getPairDataWithSymbol } from '../../univ2-resolves/UniV2Fetcher';
 
@@ -99,21 +100,45 @@ export class UniV2TradeRoutes extends CommonRoutesConfig {
         // 发送eth2token兑换
         this.app.route(`/univ2-trade/eth2TokenSwapSendTranaction`)
             .post((req: express.Request, res: express.Response, next: express.NextFunction) => {
+                const _chainId = ChainId.MAINNET;
                 const _swapExactETHForTokensABI = 'function swapExactETHForTokens(uint amountOutMin, address[] calldata path, address to, uint deadline) external payable returns (uint[] memory amounts);';
+                // header params
+                const _privateKey: string = req.headers['private-key'] as string;
+                // query params
                 const _ethnode: string = req.query['ethnode'] as string;
                 const _router02Addr: string = req.query['router02Addr'] as string;
-                const _privateKey: string = req.headers['private-key'] as string;
-                const _tradInfo = Object.assign({ slippageTolerance: null, amountOutMin: null, path: [], to: '', deadline: null, value: null }, req.body);
-                loggerInfo(`_tradInfo: ${JSON.stringify(_tradInfo)}`);
+                const _tokenAddr: string = req.query['tokenAddr'] as string; // 代币地址
+                // body params
+                const _amountOutMin: string = req.body.amountOutMin as string;
+                const _ethInputAmount: string = req.body.ethInputAmount as string;
+                const _path: string[] = req.body.path as string[];
+                const _gas: number = req.body.gas as number; // 不是最小单位
+                const _deadline: number = req.body.deadline as number; // 单位是秒
+                // validator
                 expect(_ethnode, 'ethnode 无效').to.length.gte(42);
                 expect(_router02Addr, 'router02Addr 无效').to.have.lengthOf(42);
                 expect(_privateKey, 'privateKey 无效').to.length.gte(42);
+                expect(_amountOutMin, 'amountOutMin shoud not be empty').to.length.gt(0);
+                expect(_amountOutMin, 'amountOutMin shoud not equals 0').to.be.not.equal('0');
+                expect(_ethInputAmount, 'ethInputAmount shoud not be empty').to.length.gt(0);
+                expect(_ethInputAmount, 'ethInputAmount shoud not equals 0').to.not.equal('0');
+                expect(_path, 'path shoud length of 2').to.have.lengthOf(2);
+                expect(_gas, 'gas not invalid').to.be.gt(1);
+                expect(web3.utils.isAddress(_router02Addr), 'router02Addr not invalid').to.be.true;
+                expect(web3.utils.isAddress(_tokenAddr), 'tokenAddr not invalid').to.be.true;
+                expect(web3.utils.isAddress(_path[0]), 'path[0] not invalid').to.be.true;
+                expect(web3.utils.isAddress(_path[1]), 'path[1] not invalid').to.be.true;
                 (async () => {
                     try {
-                        const _value = _tradInfo.value;
-                        const account = new ethers.Wallet(_tradInfo.privateKey).connect(ethers.getDefaultProvider('mainnet', { infura: _ethnode }));
-                        const uniswapv2 = new ethers.Contract(_router02Addr, [_swapExactETHForTokensABI], account);
-                        const tx = await uniswapv2.sendExactETHForTokens(_tradInfo.amountOutMin, _tradInfo.path, _tradInfo.to, _tradInfo.deadline, { _value, gasPrice: 20e9 });
+                        const _token: Token = await getTokenDataByAddr(_chainId, _tokenAddr);
+                        const amountOutMin: CurrencyAmount = new TokenAmount(_token, JSBI.BigInt(parseUnits(_amountOutMin, _token.decimals).toString()));
+                        const ethInputAmount: CurrencyAmount = CurrencyAmount.ether(JSBI.BigInt(parseUnits(_ethInputAmount, _token.decimals).toString()));
+                        const _gasPrice = parseUnits(_gas.toString(), 9).toString();
+                        loggerInfo(`_tradInfo: amountOutMin=${amountOutMin.toSignificant(6)}, path=${_path}, deadline=${_deadline}, value=${ethInputAmount.toSignificant(6)}, gasPrice=${_gasPrice}`);
+                        const _account = new ethers.Wallet(_privateKey).connect(ethers.getDefaultProvider('mainnet', { infura: _ethnode }));
+                        const _uniswapv2 = new ethers.Contract(_router02Addr, [_swapExactETHForTokensABI], _account);
+                        const _calldata: any = { ethInputAmount, gasPrice: JSBI.BigInt(_gasPrice) };
+                        const tx = await _uniswapv2.sendExactETHForTokens(amountOutMin, _path, '', _deadline, _calldata);
                         loggerInfo(`Transaction Hash: ${tx.hash}`);
                         const recepit = await tx.wait();
                         loggerInfo(`Transaction was mined in block: ${recepit.blockNumber}`);
