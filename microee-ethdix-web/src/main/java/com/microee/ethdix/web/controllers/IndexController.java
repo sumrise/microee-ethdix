9962af39f59160ac7277685a5a003361b1e4d1cf
zhangchunhui <zhangchunhui@huobi.com>
2021-01-27 11:10:30 +0800

    a
    a

--- microee-ethdix-web/src/main/java/com/microee/ethdix/web/controllers/IndexController.java
+++ microee-ethdix-web/src/main/java/com/microee/ethdix/web/controllers/IndexController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/")
 public class IndexController {
 
-    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    @RequestMapping(value = {"/index", "/"}, method = RequestMethod.GET)
     public String index() {
         return "index";
     }
