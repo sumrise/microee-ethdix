db694f3402b7d71bf0975ad1c6e7ed30d563a57d
zhangchunhui <zhangchunhui@huobi.com>
2021-01-27 17:44:24 +0800

    a
    a

--- uniswap-dixx/common/error.ts
+++ uniswap-dixx/common/error.ts
@@ -16,16 +16,16 @@ export class ErrorHandler extends Error {
     if (err instanceof AssertionError) {
       const code = 413;
       const { message } = err;
-      res.status(200).json({ code: code, message: `[uniswap-dixx]\`${message.split(':')[0]}\`` });
+      res.status(200).json({ code: code, message: `[uniswap-dixx](${message.split(':')[0]})` });
       return;
     }
     if (err instanceof ErrorHandler) {
       const { code, message } = err;
       loggerError(`errorCode=${code}, errorMessage=${message}`);
-      res.status(200).json({ code: code, message: `[uniswap-dixx]\`${message}\`` });
+      res.status(200).json({ code: code, message: `[uniswap-dixx](${message})` });
       return;
     }
-    loggerError(`type=${typeof err}, errorCode=${errCode}, errorMessage=${err.message}, stack=${err.stack}`);
-    res.status(errCode).json({ code: errCode, message: `[uniswap-dixx]\`${err.message}\`` });
+    loggerError(`type=${typeof err}, errorCode=${errCode}, errorMessage=${err.message || err.stack?.toString().split('\n')[0]}, stack=${err.stack}`);
+    res.status(errCode).json({ code: errCode, message: `[uniswap-dixx](${err.message || err.stack?.toString().split('\n')[0]})` });
   }
 }
