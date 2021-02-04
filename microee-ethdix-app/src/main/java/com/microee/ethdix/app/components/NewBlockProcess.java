21b9e19a51545ade75c6c8fc545a863753e561dc
zhangchunhui <zhangchunhui@huobi.com>
2021-02-04 17:40:52 +0800

    a
    a

--- microee-ethdix-app/src/main/java/com/microee/ethdix/app/components/NewBlockProcess.java
+++ microee-ethdix-app/src/main/java/com/microee/ethdix/app/components/NewBlockProcess.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import com.microee.ethdix.app.service.block.ETHBlockService;
 import com.microee.ethdix.oem.eth.enums.ChainId;
+import com.microee.plugin.response.R;
+import com.microee.plugin.response.exception.RestException;
 import com.microee.stacks.kafka.support.KafkaStringProducer;
 import okhttp3.HttpUrl;
 
@@ -29,7 +31,15 @@ public class NewBlockProcess {
 
     public void onProcessNewBlock(ChainId chainId, HttpUrl endpoint, Long blockNumber, Long timestamp) {
         logger.info("新块产生: chainId={}, url={}, newBlockNumber={}, dateTime={}", chainId.name, endpoint.toString(), blockNumber, new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date(timestamp)));
-        ethBlockService.ethGetBlockByNumber(chainId, blockNumber);
+        try {
+            if (ethBlockService.ethGetBlockByNumber(chainId, blockNumber) == null) {
+                logger.warn("没查到新块: chainId={}, url={}, newBlockNumber={}", chainId.name, endpoint.toString(), blockNumber);
+            }
+        } catch (RestException e) {
+            if (e.getCode() == R.TIME_OUT) {
+                logger.warn("查询超时: chainId={}, url={}, newBlockNumber={}", chainId.name, endpoint.toString(), blockNumber);
+            }
+        }
         kafkaStringProducer.sendMessage(newETHBlockTopic, new JSONObject().put("chainId", chainId.code).put("blockNumber",blockNumber).put("timestamp", timestamp).toString());
     }
     
