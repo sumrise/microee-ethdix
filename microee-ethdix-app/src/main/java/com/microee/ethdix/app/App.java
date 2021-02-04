dd76d9a623972c7cec730e7a1968981c3f931b0e
zhangchunhui <zhangchunhui@huobi.com>
2021-02-04 20:16:12 +0800

    a
    a

--- microee-ethdix-app/src/main/java/com/microee/ethdix/app/App.java
+++ microee-ethdix-app/src/main/java/com/microee/ethdix/app/App.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import com.microee.stacks.es.config.ElasticSearchEnabled;
 import com.microee.stacks.kafka.config.KafkaEnabled;
-import com.microee.stacks.mongodb.config.MongoEnabled;
 import com.microee.stacks.starter.MainApp;
 
 @EnableDiscoveryClient
@@ -15,7 +14,7 @@ import com.microee.stacks.starter.MainApp;
 @ComponentScan(basePackages = {"com.microee"})
 @EnableFeignClients(basePackages = {"com.microee.**.rmi", "com.uniswap.**.rmi"})
 @SpringBootApplication
-@MongoEnabled()
+//@MongoEnabled()
 @KafkaEnabled()
 @ElasticSearchEnabled()
 public class App extends MainApp {
